d10d502e606d0232662a771742c40a154a6fd840
Kot <konstantin@MacBook-Air-Kotya-4.local>
2026-04-28 16:31:31 +0300

    feat(system): add Control Plane phone-home callback service

--- apps/backend/src/system/system.module.ts
+++ apps/backend/src/system/system.module.ts
@@ -2,16 +2,28 @@ import { Module } from "@nestjs/common";
 import { EnabledModulesProvider } from "../modules/enabled/enabled-modules.provider";
 import { SystemSettingEnabledModulesProvider } from "../modules/enabled/system-setting-enabled-modules.provider";
 import { FileLicenseStateProvider } from "../modules/license/file-license-state.provider";
+import { LicenseServerProvider } from "../modules/license/license-server.provider";
 import { LicenseStateProvider } from "../modules/license/license-state.provider";
 import { ModuleStateService } from "../modules/module-state.service";
+import { ControlPlanePhoneHomeService } from "./control-plane-phone-home.service";
 import { SystemController } from "./system.controller";
 import { SystemModulesEnabledWriteService } from "./system-modules-enabled-write.service";
 import { SystemReleaseService } from "./system-release.service";
+
+const activeLicenseProviderClass =
+  process.env.LICENSE_MODE === "server" ? LicenseServerProvider : FileLicenseStateProvider;
+
+console.log(
+  `[LicenseModule] Active provider: ${
+    process.env.LICENSE_MODE === "server" ? "LicenseServerProvider" : "FileLicenseStateProvider"
+  }`,
+);
 
 @Module({
   controllers: [SystemController],
   providers: [
     ModuleStateService,
+    ControlPlanePhoneHomeService,
     SystemReleaseService,
     SystemModulesEnabledWriteService,
     {
@@ -20,7 +32,7 @@ import { SystemReleaseService } from "./system-release.service";
     },
     {
       provide: LicenseStateProvider,
-      useClass: FileLicenseStateProvider,
+      useClass: activeLicenseProviderClass,
     },
   ],
   exports: [ModuleStateService],
